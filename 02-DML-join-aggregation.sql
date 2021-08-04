6f6e2e0b84c09b619cbe05132b982551942fe2dd
서지원 <qrd7079@gmail.com>
2021-08-04 16:16:08 +0900

    join 실습문제 풀이

--- 02-DML-join-aggregation.sql
+++ 02-DML-join-aggregation.sql
@@ -124,28 +124,113 @@ WHERE emp.manager_id = man.employee_id (+) -- LEFT OUTER JOIN
 ORDER BY emp.employee_id;      
 
 -- 조인 연습문제
--- 문제1.직원들의사번(employee_id), 이름(firt_name), 성(last_name)과부서명(department_name)을 조회하여 
+-- 문제1.직원들의사번(employee_id), 이름(firt_name), 성(last_name)과 부서명(department_name)을 조회하여 
 -- 부서이름(department_name) 오름차순, 사번(employee_id) 내림차순으로정렬하세요.(106건)
+select EMPLOYEE_ID, FIRST_NAME, LAST_NAME, department_name
+from employees emp join departments dept
+    on emp.department_id = dept.department_id
+order by dept.department_name asc, emp.employee_id desc;
 
 
-
--- 문제2.employees 테이블의job_id는 현재의 업무아이디를 가지고 있습니다.
--- 직원들의사번(employee_id), 이름(firt_name), 급여(salary), 부서명(department_name), 현재업무( job_title)를 사번(employee_id) 오름차순으로 정렬하세요.
+-- 문제2.employees 테이블의 job_id 는 현재의 업무아이디를 가지고 있습니다.
+-- 직원들의사번(employee_id), 이름(firt_name), 급여(salary), 부서명(department_name), 
+-- 현재업무( job_title)를 사번(employee_id) 오름차순으로 정렬하세요.
 -- 부서가없는 Kimberely(사번178)은 표시하지 않습니다.(106건)
+select employee_id, FIRST_NAME, salary, department_name, job_title
+from employees emp 
+    inner join departments dept
+        on emp.department_id = dept.department_id
+    inner join jobs job
+        on emp.job_id = job.job_id   
+where emp.department_id is not null
+order by emp.employee_id asc;
 
 -- 문제2-1.문제2에서부서가없는Kimberely(사번178)까지표시해보세요(107건)
+select employee_id, FIRST_NAME, salary, department_name, job_title
+from employees emp 
+    FULL OUTER JOIN departments dept
+        on emp.department_id = dept.department_id
+    inner join jobs job
+        on emp.job_id = job.job_id 
+order by emp.employee_id asc;
 
 
+-- 문제3.도시별로 위치한 부서들을 파악하려고 합니다.
+-- 도시아이디, 도시명, 부서명, 부서아이디를 도시아이디(오름차순)로 정렬하여 출력하세요 
+-- 부서가없는도시는표시하지않습니다.(27건)
+select dept.location_id, city, department_name, department_id
+from departments dept join locations loc
+    on dept.location_id = loc.location_id
+where dept.location_id is not null
+order by department_id asc;
+
+-- 문제3-1.문제3에서부서가없는도시도표시합니다.(43건)
+select dept.location_id, city, department_name, department_id
+from departments dept full outer join locations loc
+    on dept.location_id = loc.location_id
+order by department_id asc;
+
+-- 문제4.지역(regions)에 속한 나라들을 지역이름(region_name), 나라이름(country_name)으로
+-- 출력하되지역이름(오름차순), 나라이름(내림차순) 으로정렬하세요.(25건)
+select region_name, country_name
+from regions reg join countries cou
+    on reg.region_id = cou.region_id
+ORDER BY reg.region_name asc, cou.country_name desc;
 
 
--- 문제3.도시별로위치한부서들을파악하려고합니다.도시아이디, 도시명, 부서명, 부서아이디를도시아이디(오름차순)로정렬하여출력하세요부서가없는도시는표시하지않습니다.(27건)문제3-1.문제3에서부서가없는도시도표시합니다.(43건)
--- 문제4.지역(regions)에속한나라들을지역이름(region_name), 나라이름(country_name)으로출력하되지역이름(오름차순), 나라이름(내림차순) 으로정렬하세요.(25건)\
--- 문제5. 자신의매니저보다채용일(hire_date)이빠른사원의사번(employee_id), 이름(first_name)과채용일(hire_date), 매니저이름(first_name), 매니저입사일(hire_date)을조회하세요.(37건)
--- 문제6.나라별로어떠한부서들이위치하고있는지파악하려고합니다.나라명, 나라아이디, 도시명, 도시아이디, 부서명, 부서아이디를나라명(오름차순)로정렬하여출력하세요.값이없는경우표시하지않습니다.(27건)
--- 문제7.job_history 테이블은과거의담당업무의데이터를가지고있다.과거의업무아이디( job_id)가‘AC_ACCOUNT’로근무한사원의사번, 이름(풀네임), 업무아이디, 시작일, 종료일을출력하세요.이름은first_name과last_name을합쳐출력합니다.(2건)
--- 문제8.각부서(department)에대해서부서번호(department_id), 부서이름(department_name), 매니저(manager)의이름(first_name), 위치(locations)한도시(city), 나라(countries)의이름(countries_name) 그리고지역구분(regions)의이름(resion_name)까지전부출력해보세요.(11건)
--- 문제9.각사원(employee)에 대해서 사번(employee_id), 이름(first_name), 부서명(department_name), 매니저(manager)의이름(first_name)을조회하세요. 
+-- 문제5. 자신의 매니저보다 채용일(hire_date)이 빠른사원의
+-- 사번(employee_id), 이름(first_name)과 채용일(hire_date), 
+-- 매니저이름(first_name), 매니저입사일(hire_date)을 조회하세요.(37건)
+select emp.employee_id, emp.first_name, emp.hire_date "자신의 채용일", 
+        man.first_name "매니저이름", man.hire_date "매니저 채용일"
+FROM employees emp JOIN employees man
+    on emp.manager_id = man.employee_id -- 여기 설정하는거 주의
+where TO_DATE(emp.hire_date, 'YYYY-MM-DD') < TO_DATE( man.hire_date, 'YYYY-MM-DD') ;
+
+-- 문제6.나라별로 어떠한 부서들이 위치하고 있는지 파악하려고 합니다. 
+-- 나라명, 나라아이디, 도시명, 도시아이디, 부서명, 부서아이디를 나라명(오름차순)로 정렬하여 출력하세요.
+-- 값이없는 경우 표시하지 않습니다.(27건)
+select cou.COUNTRY_NAME, cou.COUNTRY_ID,
+        loc. CITY, loc.LOCATION_ID, 
+        dept.DEPARTMENT_NAME, dept.DEPARTMENT_ID
+from countries cou 
+        inner join locations loc
+            on cou.country_id = loc.country_id
+        inner join departments dept
+            on dept.location_id = loc.location_id
+order by cou.COUNTRY_NAME asc;      
+      
+
+-- 문제7.job_history 테이블은 과거의 담당업무의 데이터를 가지고있다.
+-- 과거의 업무아이디(job_id)가 ‘AC_ACCOUNT’로 근무한 사원의 사번, 이름(풀네임), 업무아이디, 시작일, 종료일을출력하세요.
+-- 이름은first_name과 last_name을 합쳐 출력합니다.(2건)
+select emp.EMPLOYEE_ID, (emp.FIRST_NAME || ' ' || emp.LAST_NAME) "이름", 
+        jh.JOB_ID, jh.START_DATE, jh.END_DATE
+from  job_history jh join employees emp
+     on emp.employee_id = jh.employee_id
+where jh.job_id = 'AC_ACCOUNT';
+
+
+-- 문제8.각부서(department)에 대해서 부서번호(department_id), 부서이름(department_name), 
+-- 매니저(manager)의 이름(first_name), 위치(locations)한 도시(city), 나라(countries)의 이름(countries_name)
+-- 그리고 지역구분(regions)의 이름(resion_name)까지 전부 출력해보세요.(11건)
+select dept.department_id, dept.department_name, 
+        emp.first_name, loc.city, cou.country_name, re.region_name
+from DEPARTMENTS dept 
+    join employees emp on dept.manager_id = emp.employee_id
+    join locations loc on dept.location_id = loc.location_id
+    join countries cou on loc.country_id = cou.country_id
+    join regions re on cou.region_id = re.region_id;
+    
+
+-- 문제9.각사원(employee)에 대해서 
+-- 사번(employee_id), 이름(first_name), 부서명(department_name), 매니저(manager)의 이름(first_name)을조회하세요. 
 -- 부서가없는직원(Kimberely)도표시합니다.(106명)
+select emp.employee_id, emp.first_name, dept.department_name, man.first_name
+from employees emp full outer join departments dept
+    on emp.department_id = dept.department_id
+    join employees man on dept.manager_id = man.employee_id;
+    
 
 --------------
 -- 집계 함수
@@ -209,7 +294,7 @@ HAVING AVG(salary) >= 7000
 ORDER BY department_id;
 
 
--- 실습문제
+--집계함수 실습문제
 --문제1.매니저가 있는 직원은 몇명입니까? 아래의 결과가 나오도록 쿼리문을 작성하세요
 --문제2.직원중에 최고임금(salary)과 최저임금을 “최고임금, “최저임금” 프로젝션타이틀로 함께 출력해보세요.
 --두임금의차이는얼마인가요? “최고임금–최저임금”이란타이틀로함께출력해보세요.
