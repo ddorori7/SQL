e76c1941455896149191283466f2e45de3605fa2
서지원 <qrd7079@gmail.com>
2021-08-17 18:58:56 +0900

    Users

--- bituser.sql
+++ bituser.sql
@@ -16,3 +16,23 @@ VALUES( seq_emaillist_pk.NEXTVAL, '지원', '서', 'qrd7079@gmail.com');
 SELECT * FROM emaillist;
 
 commit;
+
+
+CREATE TABLE users (
+    no number primary key,
+    name varchar2(20) NOT NULL,
+    password varchar2(20) NOT NULL,
+    email varchar2(128) NOT NULL UNIQUE,
+    gender char(1) CHECK(gender IN ('M', 'F')),
+    created_at date DEFAULT sysdate);
+    
+CREATE SEQUENCE seq_users_pk
+    START WITH 1
+    INCREMENT BY 1;
+    
+INSERT INTO users (no, name, password, email, gender)
+VALUES(seq_users_pk.nextval, '관리자', '12345678', 'admin@example.com', 'M');
+
+select * from users;
+
+commit;
